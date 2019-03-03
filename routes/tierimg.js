a4831ed422a167beba6976d6169339c6ec3ecdc6
Kirby <rosekatkirby@gmail.com>
2019-03-03 11:55:45 -0800

    made dynamic image creation one script for better error readout

--- routes/tierimg.js
+++ routes/tierimg.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var draw = require('../tier/tier.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    //res.setHeader('Content-Type', 'image/png'); //'image/png');
 
-    res.setHeader('Content-Type', 'image/png'); //'image/png');
-//console.log(draw());
+
+/*
+
     //draw().pngStream().pipe(res);
     var initDraw = draw();
     initDraw.then((canvas) => {
@@ -13,6 +15,42 @@ router.get('/', function(req, res, next) {
         console.log(canvas);
         canvas.pngStream().pipe(res);
     });
+
+*/
+
+
+    const { createCanvas, loadImage } = require('canvas')
+    const canvas = createCanvas(200, 200)
+    const ctx = canvas.getContext('2d')
+
+    // Write "Awesome!"
+    ctx.font = '30px Impact'
+    ctx.rotate(0.1)
+    ctx.fillText('Awesome?!', 50, 100)
+
+    // Draw line under text
+    var text = ctx.measureText('Awesome?!')
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+    ctx.beginPath()
+    ctx.lineTo(50, 102)
+    ctx.lineTo(50 + text.width, 102)
+    ctx.stroke()
+
+    //console.log(document.createElement("div"));
+    //html2canvas(document.querySelector("#wrapper")).then(canvas => {
+    //    console.log(canvas);
+    //});
+
+    //add image
+
+    loadImage('http://leaderboard.kingofthehat.com/content/portraits/Portrait_Zoe.png').then((image) => {
+      ctx.drawImage(image, 50, 0, 70, 70)
+
+      //console.log('<img src="' + canvas.toDataURL() + '" />')
+      canvas.pngStream().pipe(res);
+    });
+
+
     //console.log("Hello");
     //res.render('index', { title: 'Express' });
     //res.sendFile('tier.js', { root: __dirname + '/../tier' }); //{ root: __dirname }
