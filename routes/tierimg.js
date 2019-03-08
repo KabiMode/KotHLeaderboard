39898f188df8524e1898dde2eace78460db18c13
Kirby <rosekatkirby@gmail.com>
2019-03-08 15:58:56 -0800

    Attempted to make tier by cloudflare cached through headers

--- routes/tierimg.js
+++ routes/tierimg.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var draw = require('../tier/tier.js');
 /* GET home page. */
 router.get('/:metacode', function(req, res, next) {
-    res.setHeader('Content-Type', 'image/png'); //'image/png');
+    res.setHeader('Content-Type', 'image/png');
+    res.set('Cache-Control', 'max-age=604800');
     var initDraw = draw(req.params.metacode);
     initDraw.then((canvas) => {
         console.log(canvas);
@@ -11,7 +12,8 @@ router.get('/:metacode', function(req, res, next) {
     });
 });
 router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'image/png'); //'image/png');
+    res.setHeader('Content-Type', 'image/png');
+    res.set('Cache-Control', 'max-age=604800');
     var initDraw = draw("124231-76A298B1053C4");// TODO: this is an interim solution for the until the agreed on meta can be pragmatically generated in an external json file
     initDraw.then((canvas) => {
         console.log(canvas);
